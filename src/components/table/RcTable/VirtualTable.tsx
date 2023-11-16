fb9e4ad594b90a84c55989c57f6080d149d35750
OluwadahunsiA <tommydate0@gmail.com>
2023-11-16 14:59:58 +0300

    fix rctable

--- src/components/table/RcTable/VirtualTable.tsx
+++ src/components/table/RcTable/VirtualTable.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { genVirtualTable } from 'rc-table';
 import type { InternalTableProps } from '../InternalTable';
 
--- src/components/table/RcTable/index.tsx
+++ src/components/table/RcTable/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { genTable } from 'rc-table';
 import type { InternalTableProps } from '../InternalTable';
 
